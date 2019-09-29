ddfa190a45efcf57fb569765453aafa17b9b50b5
Cecilia Leon <leoncecilia17@gmail.com>
2019-09-28 20:45:14 -0500

    Updates

---
+++ precache-manifest.de512423d72e7359e6e6fd1a81c5eef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d180e5c15abc6808650e381c69fab4c1",
+    "url": "/deployedclicky/index.html"
+  },
+  {
+    "revision": "ba6356093e7bc2c871b5",
+    "url": "/deployedclicky/static/css/main.78ce2d69.chunk.css"
+  },
+  {
+    "revision": "f5f73154a1956c611bf8",
+    "url": "/deployedclicky/static/js/2.365bb623.chunk.js"
+  },
+  {
+    "revision": "ba6356093e7bc2c871b5",
+    "url": "/deployedclicky/static/js/main.d346daf7.chunk.js"
+  },
+  {
+    "revision": "f2eb2195698514152b54",
+    "url": "/deployedclicky/static/js/runtime~main.399cad5f.js"
+  }
+]);
